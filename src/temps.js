56de802b44e8e472fd22914df245e2a4945588b6
Tazhnae Brooks <tazhnae.brooks@salesforce.com>
2019-12-31 13:49:55 -0800

    lemon comit

--- src/temps.js
+++ src/temps.js
@@ -3,6 +3,17 @@ import {Dropdown} from '@salesforce/design-system-react';
 import {IconSettings} from '@salesforce/design-system-react';
 
 
+var roleoptions= [
+		{ label: 'UTC', type: 'header'},
+		{ label: 'time', value: '0:30' },
+		{ label: 'time', value: '1:30' },
+		// { type: 'divider' },
+		// { label: 'Eastern', type: 'header'},
+		// { label: 'Menu Item Six', value: 'F0' },
+		// { label: 'Menu Item Seven', value: 'G0' },
+]
+// Element.textContent = roleoptions.join('\n')
+
 class Temps extends Component{
 	constructor(props){
 		super(props);
@@ -16,6 +27,7 @@ class Temps extends Component{
 		this.setState({selectedValue: value})
 	}
 	
+
     render(){
 		
         return(
@@ -30,18 +42,11 @@ class Temps extends Component{
 							iconVariant="border-filled"
 							onSelect={(selected) => {
 								this.handleSelectChange(selected.value);
+								Element.textContent = roleoptions.join('\n')
 							}}
-							options={[
-								{ label: 'UTC', value: '0:30' },
-								{ label: '', value: '1:30' },
-								{ label: '', value: '2:30' },
-								{ label: '', value: '3:30' },
-								{ type: 'divider' },
-								{ label: 'Eastern', value: 'E0' },
-								{ label: 'Menu Item Six', value: 'F0' },
-								{ label: 'Menu Item Seven', value: 'G0' },
-							]}
+							options={roleoptions}
 				/>
+				
 				{this.state.selectedValue}
 
 		</IconSettings>
@@ -49,7 +54,6 @@ class Temps extends Component{
             
         );
     }
-
 }
 
 export default Temps;
