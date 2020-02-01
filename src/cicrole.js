bac0a91d44b3819000b023205cd0a3b56f29bef5
Tazhnae Brooks <tazhnae.brooks@salesforce.com>
2020-01-31 18:24:32 -0800

    custard-apple commit

--- src/cicrole.js
+++ src/cicrole.js
@@ -5,8 +5,6 @@ import { IconSettings } from '@salesforce/design-system-react';
 import './style.css';
 
 
-
-
 // const Grid = () => (
 // 	<DataTableCell >
 // 		<div class="slds-form-element">
@@ -23,13 +21,6 @@ class Grid extends Component {
 	constructor(props) {
 		super(props);
 		// const chip = [];
-		// for (var i = 0; i < 29; i++) {
-		// 	chip.push(
-		// 		{
-		// 			potato: chance.number()
-		// 		}
-		// 	);
-		// }
 		this.state = {
 			col: props.col,
 			row: props.row,
@@ -37,13 +28,27 @@ class Grid extends Component {
 		}
 	}
 
+	// Snack(props) {
+	// 	props.chips.forEach((chip, i) => {
+	// 		if ((i + 1) % 1 == 0) {
+	// 			potato.push(
+	// 				<div key={chip.id}>
+	// 					<h1 key={chip.id} ></h1>
+	// 				</div>
+	// 				// potato: props.number()
+	// 				// columns.map(row => Grid({ row })) //fix
+	// 			);
+	// 		}
+	// 	});
+	// }
+
 
 	render() {
 		return (
 			<DataTableCell >
 				<div class="slds-form-element">
 					<div class="slds-form-element__control" >
-						<input type="text" id="text-input-id-1" required="" class="slds-input" value={this.state.col + ":" + this.state.row} /> {/* value={this.state.col + " " + this.state.row}*/}
+						<input type="text" id="text-input-id-1" required="" class="slds-input" /> {/* value={this.state.col + " " + this.state.row}, //value={potato} */}
 					</div>
 				</div>
 			</DataTableCell >
@@ -56,35 +61,33 @@ Grid.displayName = DataTableCell.displayName;
 
 
 //role table
-const columns = [
+// const columns = [
 
 
 
-	<DataTableColumn key="subrole1" label="IC" property="ic" width="10rem"><Grid col={1} row={1} /></DataTableColumn>, /*<Grid col={1} row={1} /> */,
+// 	< DataTableColumn key="" label="IC" property="ic" width="10rem" ><Grid /></DataTableColumn >, /*<Grid col={1} row={1} /> */,
 
-	<DataTableColumn key="subrole2" label="BO" property="bo" width="10rem"><Grid col={2} row={1} /></DataTableColumn>,
+// 	// <DataTableColumn key="subrole2" label="BO" property="bo" width="10rem"><Grid /></DataTableColumn>,
 
-	<DataTableColumn key="subrole3" label="LNO" property="lno" width="10rem"><Grid /></DataTableColumn>,
+// 	// <DataTableColumn key="subrole3" label="LNO" property="lno" width="10rem"><Grid /></DataTableColumn>,
 
-	<DataTableColumn key="subrole4" label="Scribe" property="scribe" width="10rem" ><Grid /></DataTableColumn>,
+// 	// <DataTableColumn key="subrole4" label="Scribe" property="scribe" width="10rem" ><Grid /></DataTableColumn>,
 
-	<DataTableColumn key="subrole5" label="OR" property="or" width="10rem" ><Grid /></DataTableColumn>,
+// 	// <DataTableColumn key="subrole5" label="OR" property="or" width="10rem" ><Grid /></DataTableColumn>,
 
-	<DataTableColumn key="subrole6" label="IC" property="singleic" width="10rem" ><Grid /></DataTableColumn>,
+// 	// <DataTableColumn key="subrole6" label="IC" property="singleic" width="10rem" ><Grid /></DataTableColumn>,
 
-	<DataTableColumn key="subrole7" label="Ex Esc" property="exec" width="10rem"><Grid /></DataTableColumn>,
+// 	// <DataTableColumn key="subrole7" label="Ex Esc" property="exec" width="10rem"><Grid /></DataTableColumn>,
 
-	<DataTableColumn key="subrole8" label="PRI" property="ucpri" width="10rem" ><Grid /></DataTableColumn>,
+// 	// <DataTableColumn key="subrole8" label="PRI" property="ucpri" width="10rem" ><Grid /></DataTableColumn>,
 
-	<DataTableColumn key="subrole9" label="SEC" property="ucsec" width="10rem" ><Grid /></DataTableColumn>,
+// 	// <DataTableColumn key="subrole9" label="SEC" property="ucsec" width="10rem" ><Grid /></DataTableColumn>,
 
-	<DataTableColumn key="subrole10" label="PRI" property="ecommpri" width="10rem" ><Grid /></DataTableColumn>,
+// 	// <DataTableColumn key="subrole10" label="PRI" property="ecommpri" width="10rem" ><Grid /></DataTableColumn>,
 
-	<DataTableColumn key="subrole11" label="SEC" property="ecommsec" width="10rem"><Grid /></DataTableColumn>,
+// 	// <DataTableColumn key="subrole11" label="SEC" property="ecommsec" width="10rem"><Grid /></DataTableColumn>,
 
-
-
-];
+// ];
 // var ROWS = []
 
 class Role extends Component {
@@ -96,9 +99,10 @@ class Role extends Component {
 		this.displayName = 'roles'
 	}
 
+
 	componentDidMount() {
 		var rows = [];
-		for (var i = 1; i <= 29; i++) {
+		for (var i = 1; i <= 3; i++) {
 			rows.push(
 				{
 					row: i,
@@ -212,12 +216,13 @@ class Role extends Component {
 						items={this.state.row}
 						id="roles-columnBordered"
 						noRowHover
+					// assitiveText={{ selectRow: '' }}
 
 					>
 
-						{/* <DataTableColumn><Grid col={1} row={1} /></DataTableColumn> */}
+						< DataTableColumn key="" label="IC" property="ic" width="10rem" ><Grid /></DataTableColumn >
 						{/* {ROWS} */}
-						{columns}
+						{/* {columns} */}
 					</DataTable>
 				</IconSettings >
 			</div >
