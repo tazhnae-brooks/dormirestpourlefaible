2a2b822adda829017187f6c775642c43dbbbf93f
Tazhnae Brooks <tazhnae.brooks@salesforce.com>
2020-02-19 18:30:56 -0800

    squash commit

--- src/index.js
+++ src/index.js
@@ -24,30 +24,21 @@ class Index extends Component {
     };
 
     componentDidMount() {
-        //     // Call our fetch function below once the component mounts
-        //     this.callBackendAPI()
-        //         .then(res => this.setState({ data: res.express }))
-        //         .catch(err => console.log(err));
-        // }
-        // // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-        // callBackendAPI = async () => {
-        //     const response = await fetch('/express_backend');
-        //     const body = await response.json();
-
-        //     if (response.status !== 200) {
-        //         throw Error(body.message)
-        //     }
-        //     return body;
-        fetch('/express_backend')
-            .then(res => {
-                console.log(res);
-                return res.send()
-            })
-            .then(data => {
-                console.log(data);
-                this.setState({ data })
-            });
+        // Call our fetch function below once the component mounts
+        this.callBackendAPI()
+            .then(res => this.setState({ data: res.express }))
+            .catch(err => console.log(err));
     }
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+    callBackendAPI = async () => {
+        const response = await fetch('/express_backend');
+        const body = await response.json();
+
+        if (response.status !== 200) {
+            throw Error(body.message)
+        }
+        return body;
+    };
 
     render() {
         return (
