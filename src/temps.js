3e89acd14284ef655c29f031419dce67f86e053f
Tazhnae Brooks <tazhnae.brooks@salesforce.com>
2020-02-28 18:03:37 -0800

    bok choy commit

--- src/temps.js
+++ src/temps.js
@@ -4,16 +4,6 @@ import { IconSettings } from '@salesforce/design-system-react';
 import './style.css'
 
 
-const teamtimes = [
-	{ label: 'UTC', type: 'header' },
-	{ label: '0:30 - 3:30', value: '0:30 - 3:30' },
-	{ label: '4:30 - 7:30', value: '4:30 - 7:30' },
-	{ label: '8:30 - 11:30', value: '8:30 - 11:30' },
-	{ label: '12:30 - 15:30', value: '12:30 - 15:30' },
-	{ label: '16:30 - 19:30', value: '16:30 - 19:30' },
-	{ label: '20:30 - 23:30', value: '20:30 - 23:30' }
-]
-
 class Temps extends Component {
 	constructor(props) {
 		super(props);
@@ -27,18 +17,23 @@ class Temps extends Component {
 		let grid = data.grid
 		grid.forEach(gridLocation => {
 			let matchingElement = document.querySelector(`[data-location='${gridLocation}']`)
-			matchingElement.value = tz
+			matchingElement.label = tz
+
 		})
 	}
 
-	async handleSelectChange(value) {
-		const response = await fetch(`/query_time?tz=${value}`);
-		const body = await response.json();
-		console.log(body.data[0]);
-		if (response.status !== 200) {
-			throw Error(body.message)
+	async handleSelectChange(selected) {
+		let name = "pp"
+
+		// find the first grid (1:1)
+		let start_grid = selected.grid_span.start_grid.split(":").map(x => +x) // [1,1]
+		let end_grid = selected.grid_span.end_grid.split(":").map(x => +x) // [1,4]
+
+		for (let i = start_grid[1]; i <= end_grid[1]; i++) {
+			let matchingElement = document.querySelector(`[data-location='${start_grid.join(":")}']`)
+			matchingElement.value = name
+			start_grid[1]++
 		}
-		this.handleTimeData(body.data[0]);
 	}
 
 
@@ -53,9 +48,17 @@ class Temps extends Component {
 						iconPosition="right"
 						label="Temps"
 						onSelect={(selected) => {
-							this.handleSelectChange(selected.value);
+							this.handleSelectChange(selected);
 						}}
-						options={teamtimes}
+						options={[
+							{ label: 'UTC', type: 'header' },
+							{ label: '0:30 - 3:30', value: '0:30 - 3:30', grid_span: { start_grid: '1:1', end_grid: '1:4' } },
+							{ label: '4:30 - 7:30', value: '4:30 - 7:30', grid_span: { start_grid: '1:5', end_grid: '1:8' } },
+							{ label: '8:30 - 11:30', value: '8:30 - 11:30', grid_span: { start_grid: '1:9', end_grid: '1:12' } },
+							{ label: '12:30 - 15:30', value: '12:30 - 15:30', grid_span: { start_grid: '1:13', end_grid: '1:16' } },
+							{ label: '16:30 - 19:30', value: '16:30 - 19:30', grid_span: { start_grid: '1:17', end_grid: '1:20' } },
+							{ label: '20:30 - 23:30', value: '20:30 - 23:30', grid_span: { start_grid: '1:21', end_grid: '1:24' } }
+						]}
 						width="x-small"
 					/>
 					<br />
